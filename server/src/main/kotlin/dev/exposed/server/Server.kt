858eebdc863ced79ea58a3a924fdf4095c73a058
Padam Raj Gurung <padam.gurung@outlook.com>
2025-04-10 17:39:37 +0545

    DAO - CRUD Completed

--- server/src/main/kotlin/dev/exposed/server/Server.kt
+++ server/src/main/kotlin/dev/exposed/server/Server.kt
@@ -1,45 +1,23 @@
 package dev.exposed.server
 
 import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.greaterEq
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.isNull
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main(){
 
-    //Database.connect("jdbc:h2:mem:test", driver = "org.h2.driver" )
     Database.connect("jdbc:postgresql://localhost:5432/my_db", user = "postgres", password = "root" )
     println("Connected to database")
     transaction {
-        SchemaUtils.create(Users)
-        println("Table created!")
-    }
-    transaction {
-        Users.insert {
-            it[name] = "Duke"
-            it[age] = 25
+        User.new {
+            name = "Charlie"
+            age = 30
         }
-        Users.insert {
-            it[name] = "Charles"
-            it[age] = null
+
+        User.all().forEach {
+            println("${it.name} is ${it.age} years old")
         }
+        val user = User.findById(1)
+        user?.age = 11
     }
-    transaction {
-        Users.selectAll().forEach { row ->
-            println("ID: ${row[Users.id]}, Name: ${row[Users.name]}, Age: ${row[Users.age]}")
-        }
-    }
-    transaction {
-        Users.select(Users.name, Users.age) // Explicitly select columns
-            .where { Users.age greaterEq 18 }
-            .forEach {
-                println("${it[Users.name]} is an adult")
-            }
-    }
-   transaction {
-       Users.update({Users.name eq "Alice"}) {
-           it[age] = 26
-       }
-       Users.deleteWhere { Users.age.isNull() }
-   }
 }
--- server/src/main/kotlin/dev/exposed/server/Users.kt
+++ server/src/main/kotlin/dev/exposed/server/Users.kt
@@ -1,11 +1,9 @@
 package dev.exposed.server
 
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Table
 
-object Users : Table("users") {
-    val id = integer("id").autoIncrement()
+object Users : IntIdTable("users") {
     val name = varchar("name", 50)
     val age = integer("age").nullable()
-
-    override val primaryKey = PrimaryKey(id)
 }
